aafb643d235bcbd5b4fa898f23c118ee6eed7641
MouseZero <rm@mousezero.com>
2017-05-29 01:34:05 -0700

    fixed linting errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react'
 import { render } from 'react-dom'
-import { Router } from 'react-router'
-import routes from './routes'
 import App from './components/App'
 
 render(
-    App(),
-    document.getElementById('app')
+  App(),
+  document.getElementById('app')
 )
 
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,7 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import autoprefixer from 'autoprefixer';
-import path from 'path';
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import autoprefixer from 'autoprefixer'
+import path from 'path'
 
 export default {
   resolve: {
@@ -61,4 +61,4 @@ export default {
       {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']}
     ]
   }
-};
+}
