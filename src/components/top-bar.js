ed83b4fc630a5d025d33b65a8626d308dab93a4d
MouseZero <rm@mousezero.com>
2017-05-29 19:23:34 -0700

    top bar

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -3,7 +3,19 @@ import React from 'react'
 const TopBar = () => {
   return (
     <div className="top-bar">
-      <div className="title">Nightlife App</div>
+      <div className="title">
+        Nightlife App
+      </div>
+      <div className="tag-line">
+        See which bars are popular tonight and RSVP ahead of time!
+      </div>
+      <div className="nightlife-input-group">
+        <label className="nightlife-label">Search Area</label>
+        <br/>
+        <input className="nightlife-input" maxlength="100"/>
+        <br/>
+        <span>What city are you spending the evening in</span>
+      </div>
     </div>
   )
 }
