032bbe0717f73b34ab2152765fe5a3165b329fc3
MouseZero <rm@mousezero.com>
2017-05-29 21:26:04 -0700

    cellphone friendly

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -4,14 +4,12 @@ const TopBar = () => {
   return (
     <div className="top-bar">
       <div className="title">
-        Nightlife App
+        Nightlife Bar Attendance
       </div>
       <div className="tag-line">
-        See which bars are popular tonight and RSVP ahead of time!
+        See which bars are popular tonight!
       </div>
       <div className="nightlife-input-group">
-        <label className="nightlife-label">Search Area</label>
-        <br/>
         <input className="nightlife-input" maxlength="100"/>
         <br/>
         <span>What city are you spending the evening in</span>
