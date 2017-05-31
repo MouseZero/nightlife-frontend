66d82675cc9d6eba26fb3c84854d1f98df213e59
MouseZero <rm@mousezero.com>
2017-05-31 15:15:41 -0700

    (test) TopBar

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TopBar = () => {
+const TopBar = ({ searchAction }) => {
   return (
     <div className="top-bar">
       <div className="title">
@@ -10,7 +10,7 @@ const TopBar = () => {
         See which bars are popular tonight!
       </div>
       <div className="nightlife-input-group">
-        <input className="nightlife-input"/>
+        <input id="bar-search" className="nightlife-input" onSubmit={searchAction} />
         <br/>
         <span>What city are you spending the evening in</span>
       </div>
