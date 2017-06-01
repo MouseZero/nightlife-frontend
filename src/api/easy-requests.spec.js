f184cd69a7246786737e5a28a6bb2454423092c6
MouseZero <rm@mousezero.com>
2017-05-31 21:39:19 -0700

    (test) better test for easy-request

--- src/api/easy-requests.spec.js
+++ src/api/easy-requests.spec.js
@@ -1,25 +1,45 @@
-import { easyRequest } from './easy-requests'
+import { post } from './easy-requests'
 import assert from 'assert'
+import sinon from 'sinon'
 
-describe('easyRequest', () => {
-  it('is a function', () => {
-    assert(typeof easyRequest === 'function')
-  })
+describe('post', () => {
   it('returns json from fetch', async () => {
     const fetch = () => Promise.resolve({
       json: () => { return {test: "worked"} }
     })
     assert.deepEqual(
-      await easyRequest(fetch)(),
+      await post(fetch)(),
       {test: "worked"},
       'did not return json from fetch'
     )
+  })
+  it('calls fetch the correct way without token', async () => {
+    const fetch = sinon.spy((a, b) => {
+        return Promise.resolve({
+          json: () => { return {} }
+        })
+      }
+    )
+    await post(fetch)({
+      user: 'bill',
+      password: 'password'
+    }, 'https://someurl.com')
+
+    assert(fetch.calledWith('https://someurl.com', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'user=bill&password=password'
+    }))
+
   })
   it('throws error if fetch throws error', async () => {
     const fetch = () => Promise.reject(new Error('some error'))
     let error
     try {
-      await easyRequest(fetch)()
+      await post(fetch)()
     } catch (e) { error = e }
 
     if(!error) throw new Error('should have thrown an error')
