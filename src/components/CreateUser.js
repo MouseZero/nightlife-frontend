07d8e4b80d380b506f21154583e380088b0d3428
MouseZero <rm@mousezero.com>
2017-06-04 10:57:53 -0700

    password field is now a controlled component

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -3,36 +3,49 @@ import React from 'react'
 class CreateUser extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {value: ''}
-    this.clickedButton = this.clickedButton.bind(this)
-    this.onChange = this.onChange.bind(this)
+    this.state = {
+      name: '',
+      password: ''
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+    this.nameOnChange = this.nameOnChange.bind(this)
+    this.passwordOnChange = this.passwordOnChange.bind(this)
   }
 
-  clickedButton(e){
-    alert()
+  onSubmit(e){
     e.preventDefault()
+    alert(this.state.name +' '+ this.state.password)
   }
 
-  onChange(event){
-    this.setState({value: event.target.value})
+  nameOnChange(event){
+    this.setState({name: event.target.value})
+  }
+
+  passwordOnChange(event){
+    this.setState({password: event.target.value})
   }
 
   render() {
     return (
       <div className="outer-input-form">
-        <form className="input-form center-content" onSubmit={this.clickedButton}>
+        <form className="input-form center-content" onSubmit={this.onSubmit}>
           <label to="user-name-input" className="label">User Name: </label>
           <input
             id="user-name-input"
-            value={this.state.value}
-            onChange={this.onChange}
+            value={this.state.name}
+            onChange={this.nameOnChange}
             type="text"
             className="nightlife-input"
-            type="text">
-          </input>
+          />
           <label to="user-password-input" className="label">Password</label>
-          <input id="user-password-input" className="nightlife-input" type="password"></input>
-          <button id="submit" className="nightlife-button" onSubmit={this.clickedButton} >Create New User</button>
+          <input
+            id="user-password-input"
+            value={this.state.password}
+            onChange={this.passwordOnChange}
+            className="nightlife-input"
+            type="password"
+          />
+          <button id="submit" className="nightlife-button" onSubmit={this.onSubmit} >Create New User</button>
         </form>
       </div>
     )
