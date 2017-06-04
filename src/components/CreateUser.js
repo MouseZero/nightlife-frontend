f83b452454b34bf9a1e6ffc1fc90373f31283133
MouseZero <rm@mousezero.com>
2017-06-03 18:48:39 -0700

    design change on create user page

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const CreateUser = () => {
-  return (
-    <div>
-      Create user placeholder page
-    </div>
-  )
+class CreateUser extends React.Component {
+  render() {
+    return (
+      <div className="input-form">
+        <div>
+        <label to="user-name-input" className="label">User Name: </label>
+        </div>
+        <div>
+        <input id="user-name-input" type="text" className="nightlife-input" type="text"></input>
+        </div>
+        <div>
+        <label to="user-password-input" className="label">Password</label>
+        </div>
+        <div>
+        <input id="user-password-input" className="nightlife-input" type="password"></input>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default CreateUser
--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -1,23 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router'
 import PropTypes from 'prop-types'
 
 const TopBar = ({ searchAction }) => {
   return (
-    <div className="top-bar">
-      <div className="title">
-        Nightlife Bar Attendance
-      </div>
-      <div className="tag-line">
-        See which bars are popular tonight!
-      </div>
-      <div className="nightlife-input-group">
-        <input id="bar-search" className="nightlife-input" onSubmit={searchAction} />
-        <br/>
-        <span>What city are you spending the evening in</span>
-      </div>
-      <div className="login-group">
-        <button id="create-user" className="main-button">Create User</button>
-        <button id="login" className="main-button">Login</button>
+    <div id="outer-container">
+      <div className="top-bar">
+        <div className="title">
+          Nightlife Bar Attendance
+        </div>
+        <div className="tag-line">
+          See which bars are popular tonight!
+        </div>
+        <div className="nightlife-input-group">
+          <input id="bar-search" className="nightlife-input" onSubmit={searchAction} />
+          <br/>
+          <span>What city are you spending the evening in</span>
+        </div>
+        <div className="login-group">
+          <Link to="create/user">
+            Create User
+          </Link>
+        </div>
       </div>
     </div>
   )
