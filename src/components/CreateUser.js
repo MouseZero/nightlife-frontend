73ee621c4b54bddfb222bf9e7fe1bbd051602de0
MouseZero <rm@mousezero.com>
2017-06-04 11:12:27 -0700

    Moved the input to its own component for reuse.

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,55 +1,9 @@
 import React from 'react'
+import UserNamePasswordCollection from './UserNamePasswordCollection'
 
-class CreateUser extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      password: ''
-    }
-    this.onSubmit = this.onSubmit.bind(this)
-    this.nameOnChange = this.nameOnChange.bind(this)
-    this.passwordOnChange = this.passwordOnChange.bind(this)
-  }
-
-  onSubmit(e){
-    e.preventDefault()
-    alert(this.state.name +' '+ this.state.password)
-  }
-
-  nameOnChange(event){
-    this.setState({name: event.target.value})
-  }
-
-  passwordOnChange(event){
-    this.setState({password: event.target.value})
-  }
-
-  render() {
-    return (
-      <div className="outer-input-form">
-        <form className="input-form center-content" onSubmit={this.onSubmit}>
-          <label to="user-name-input" className="label">User Name: </label>
-          <input
-            id="user-name-input"
-            value={this.state.name}
-            onChange={this.nameOnChange}
-            type="text"
-            className="nightlife-input"
-          />
-          <label to="user-password-input" className="label">Password</label>
-          <input
-            id="user-password-input"
-            value={this.state.password}
-            onChange={this.passwordOnChange}
-            className="nightlife-input"
-            type="password"
-          />
-          <button id="submit" className="nightlife-button" onSubmit={this.onSubmit} >Create New User</button>
-        </form>
-      </div>
-    )
-  }
+const CreateUser = () => {
+  return (
+  <UserNamePasswordCollection buttonText="Create User"/>
+  )
 }
-
 export default CreateUser
---
+++ src/components/UserNamePasswordCollection.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class UserNamePasswordCollection extends React.Component { constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      password: ''
+    }
+    this.onSubmit = this.onSubmit.bind(this)
+    this.nameOnChange = this.nameOnChange.bind(this)
+    this.passwordOnChange = this.passwordOnChange.bind(this)
+  }
+
+  onSubmit(e){
+    e.preventDefault()
+    alert(this.state.name +' '+ this.state.password)
+  }
+
+  nameOnChange(event){
+    this.setState({name: event.target.value})
+  }
+
+  passwordOnChange(event){
+    this.setState({password: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="outer-input-form">
+        <form className="input-form center-content" onSubmit={this.onSubmit}>
+          <label to="user-name-input" className="label">User Name: </label>
+          <input
+            id="user-name-input"
+            value={this.state.name}
+            onChange={this.nameOnChange}
+            type="text"
+            className="nightlife-input"
+          />
+          <label to="user-password-input" className="label">Password</label>
+          <input
+            id="user-password-input"
+            value={this.state.password}
+            onChange={this.passwordOnChange}
+            className="nightlife-input"
+            type="password"
+          />
+          <button id="submit" className="nightlife-button" onSubmit={this.onSubmit}>{this.props.buttonText}</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+UserNamePasswordCollection.propTypes = {
+  buttonText: PropTypes.string.isRequired
+}
+
+export default UserNamePasswordCollection
