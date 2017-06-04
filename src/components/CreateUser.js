53906615618c7fb6ff1ea5ad8e8f0f64021817d9
MouseZero <rm@mousezero.com>
2017-06-03 19:35:13 -0700

    design improvements

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -3,19 +3,14 @@ import React from 'react'
 class CreateUser extends React.Component {
   render() {
     return (
-      <div className="input-form">
-        <div>
-        <label to="user-name-input" className="label">User Name: </label>
-        </div>
-        <div>
-        <input id="user-name-input" type="text" className="nightlife-input" type="text"></input>
-        </div>
-        <div>
-        <label to="user-password-input" className="label">Password</label>
-        </div>
-        <div>
-        <input id="user-password-input" className="nightlife-input" type="password"></input>
-        </div>
+      <div className="outer-input-form">
+        <span className="input-form center-content">
+          <label to="user-name-input" className="label">User Name: </label>
+          <input id="user-name-input" type="text" className="nightlife-input" type="text"></input>
+          <label to="user-password-input" className="label">Password</label>
+          <input id="user-password-input" className="nightlife-input" type="password"></input>
+          <button id="submit" className="nightlife-button">Create New User</button>
+        </span>
       </div>
     )
   }
