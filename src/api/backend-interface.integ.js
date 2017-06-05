2a7cf1d25e685bb47dd947cd77903f4e12b2054e
MouseZero <rm@mousezero.com>
2017-06-04 22:27:51 -0700

    backend interface now has a authenticate function

--- src/api/backend-interface.integ.js
+++ src/api/backend-interface.integ.js
@@ -1,15 +1,33 @@
-import { createUser } from './backend-interface'
+import {
+  createUser,
+  authenticate
+} from './backend-interface'
 import assert from 'assert'
+const BACKEND_URL = 'http://localhost:3033/'
+let TOKEN = ''
 
 describe('backend-interface', () => {
   describe('createUser', () => {
     it('Should be able to create user', async () => {
       try {
-        const result = createUser('test', 'password')
+        const result = await createUser(BACKEND_URL)('test', 'password')
         assert(result.success, 'should have created user')
       } catch (error) {
         assert(false,
         `Threw an error while trying to create user` + error)
+      }
+    })
+  })
+  describe('authenticate', () => {
+    it('authenticates user', async () => {
+      try {
+        const result = await authenticate(BACKEND_URL)('test', 'password')
+        TOKEN = result.token
+        console.log(TOKEN)
+        assert(result.success, 'should have authenticated user')
+      } catch (error) {
+        assert(false,
+        'Threw an error while trying to authenticate user')
       }
     })
   })
--- src/api/backend-interface.js
+++ src/api/backend-interface.js
@@ -2,17 +2,34 @@ import { post, get } from './easy-requests'
 import fetch from 'node-fetch'
 const doPost = post(fetch)
 const doGet = get(fetch)
-const BASEURL = 'https://nightlife-backend-fcc.herokuapp.com/'
+const BASEURL = process.env.BACKEND_URL
 
-const createUser = (userName, password) => {
+const createUser = (baseurl) => (userName, password) => {
   return doPost({
     name: userName,
     password
   },
-  BASEURL + 'users'
+  baseurl + 'users'
   )
 }
 
+const authenticate = (baseurl) => (userName, password) => {
+  return doPost({
+    user: userName,
+    password
+  },
+  baseurl + 'authenticate'
+  )
+}
+
+const backendInterface = {
+  createUser: createUser(BASEURL),
+  authenticate: authenticate(BASEURL)
+}
+
+
 export {
-  createUser
+  backendInterface as default,
+  createUser,
+  authenticate
 }
