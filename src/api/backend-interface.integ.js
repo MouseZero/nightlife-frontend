e16f4d02cbdd3f92d32e52f6e9230bd168bd5c42
MouseZero <rm@mousezero.com>
2017-06-05 18:46:05 -0700

    warning for integrated test potental issues

--- src/api/backend-interface.integ.js
+++ src/api/backend-interface.integ.js
@@ -7,39 +7,45 @@ import assert from 'assert'
 const BACKEND_URL = 'http://localhost:3033/'
 let TOKEN = ''
 
-describe('backend-interface', () => {
-  describe('createUser', () => {
-    it('Should be able to create user', async () => {
-      try {
-        const result = await createUser(BACKEND_URL)('test', 'password')
-        assert(result.success, 'should have created user')
-      } catch (error) {
-        assert(false,
-        `Threw an error while trying to create user` + error)
-      }
+/*------------ WARNING --------------
+These test have a temporal coupling meaning some of them
+have to be in order.*/
+
+context('Warning -- temporal coupling', () => {
+  describe('backend-interface', () => {
+    describe('createUser', () => {
+      it('Should be able to create user', async () => {
+        try {
+          const result = await createUser(BACKEND_URL)('test', 'password')
+          assert(result.success, 'should have created user')
+        } catch (error) {
+          assert(false,
+          `Threw an error while trying to create user` + error)
+        }
+      })
     })
-  })
-  describe('authenticate', () => {
-    it('authenticates user', async () => {
-      try {
-        const result = await authenticate(BACKEND_URL)('test', 'password')
-        TOKEN = result.token
-        assert(result.success, 'should have authenticated user')
-      } catch (error) {
-        assert(false,
-        'Threw an error while trying to authenticate user')
-      }
+    describe('authenticate', () => {
+      it('authenticates user', async () => {
+        try {
+          const result = await authenticate(BACKEND_URL)('test', 'password')
+          TOKEN = result.token
+          assert(result.success, 'should have authenticated user')
+        } catch (error) {
+          assert(false,
+          'Threw an error while trying to authenticate user')
+        }
+      })
     })
-  })
-  describe('removeUser', () => {
-    it('should delete user', async () => {
-      try {
-        const result = await removeUser(BACKEND_URL)(TOKEN)
-        assert(result.success, 'should have deleted user')
-      } catch (error) {
-        assert(false,
-        'Threw an error while trying to delete user')
-      }
+    describe('removeUser', () => {
+      it('should delete user', async () => {
+        try {
+          const result = await removeUser(BACKEND_URL)(TOKEN)
+          assert(result.success, 'should have deleted user')
+        } catch (error) {
+          assert(false,
+          'Threw an error while trying to delete user')
+        }
+      })
     })
   })
 })
