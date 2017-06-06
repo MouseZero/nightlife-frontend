87d58a50bf82a95fd18014da0480c0b3d8b9532d
MouseZero <rm@mousezero.com>
2017-06-06 14:56:59 -0700

    added login page

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -6,8 +6,13 @@ const BottomBar = () => {
     <div id="bottom-bar-container">
       <div className="login-group">
         <Link to="/create/user">
-          <button className="nightlife-button">
+          <button className="nightlife-button bottom-button">
             Create User
+          </button>
+        </Link>
+        <Link to="/login">
+          <button className="nightlife-button bottom-button">
+            Login
           </button>
         </Link>
       </div>
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
 import CreateUser from './components/CreateUser'
 import DefaultPage from './components/DefaultPage'
+import Login from './components/Login'
 
 export default (
     <Route path="/" component={App}>
       <IndexRoute component={DefaultPage}/>
       <Route path="/create/user" component={CreateUser}/>
+      <Route path="/login" component={Login}/>
     </Route>
 )
