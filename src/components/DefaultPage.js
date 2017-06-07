c30cdef1ada8858fcf1b09c4b676df04fc740489
MouseZero <rm@mousezero.com>
2017-06-07 12:55:21 -0700

    default page connected to redux

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
 
-const DefaultPage = () => {
-  return (
-    <div>Default placeholder page</div>
-  )
+class DefaultPage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    if (_.isEmpty(this.props.businesses)) {
+      return (<div>There are no search Results</div>)
+    }
+    return (
+      <div>
+        Search Worked
+      </div>
+    )
+  }
 }
 
-export default DefaultPage
+DefaultPage.propTypes = {
+  businesses: PropTypes.array
+}
+
+const mapStateToProps = (state) => {
+  return {
+    businesses: state.search.businesses
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DefaultPage)
