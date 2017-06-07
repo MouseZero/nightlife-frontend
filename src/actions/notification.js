c3e8c9befc273a800d5a2393d4670c0849d0c5f2
MouseZero <rm@mousezero.com>
2017-06-07 10:45:12 -0700

    refactor notifications

--- src/actions/notification.js
+++ src/actions/notification.js
@@ -1,3 +1,8 @@
+import { notificationTypes } from '../constants/notifications'
+const { GOOD, WARN, ERROR } = notificationTypes
+
+// TODO support for multiplue error messages
+
 const notification = (msg, notificationType = 'good') => {
   return {
     type: 'NOTIFICATION',
@@ -12,7 +17,34 @@ const notificationClear = () => {
   }
 }
 
+const _resetNotificationInTime = (dispatch) => new Promise((resolve) => {
+  setTimeout(() => {
+    dispatch(notificationClear())
+    resolve('success')
+  }, 5000)
+})
+
+const _notify = (message, notificationType, dispatch) => {
+  dispatch(notification(message, notificationType))
+  _resetNotificationInTime(dispatch)
+}
+
+const notifyGood = (message, dispatch) => {
+  _notify(message, GOOD, dispatch)
+}
+
+const notifyWarn = (message, dispatch) => {
+  _notify(message, WARN, dispatch)
+}
+
+const notifyError = (message, dispatch) => {
+  _notify(message, ERROR, dispatch)
+}
+
 export {
   notification,
-  notificationClear
+  notificationClear,
+  notifyGood,
+  notifyWarn,
+  notifyError
 }
--- src/actions/user.js
+++ src/actions/user.js
@@ -1,6 +1,5 @@
 import backendInterface from '../api/backend-interface'
-import { notificationClear, notification } from './notification'
-import { notificationTypes } from '../constants/notifications'
+import { notifyGood, notifyError } from './notification'
 
 const setToken = (token) => {
   return {
@@ -16,27 +15,18 @@ const logout = () => {
   }
 }
 
-const resetNotificationInTime = (dispatch) => new Promise((resolve) => {
-  setTimeout(() => {
-    dispatch(notificationClear())
-    resolve('success')
-  }, 5000)
-})
-
 const createUser = (userName, password) => {
   return (dispatch) => {
     backendInterface.createUser(userName, password)
     .then(({success, message}) => {
       if (success) {
-        dispatch(notification(message))
+        notifyGood(message, dispatch)
       } else {
-        dispatch(notification(message, notificationTypes.ERROR))
+        notifyError(message, dispatch)
       }
-      return resetNotificationInTime(dispatch)
     })
     .catch(() => {
-      dispatch(notification('Could not create user', notificationTypes.ERROR))
-      return resetNotificationInTime(dispatch)
+      notifyError('Could not create user', dispatch)
     })
   }
 }
@@ -46,40 +36,23 @@ const authenticate = (userName, password) => {
     backendInterface.authenticate(userName, password)
     .then(({success, token, message}) => {
       if (success) {
-        dispatch(notification('Login Successful'))
+        notifyGood('Login Successful', dispatch)
         sessionStorage.setItem('token', token)
         dispatch(setToken(token))
       } else {
         message = message || 'Could not login'
-        dispatch(notification(message), notificationTypes.ERROR)
+        notifyError(message, dispatch)
       }
-      return resetNotificationInTime(dispatch)
     })
     .catch(() => {
-      dispatch(notification('Could not login', notificationTypes.ERROR))
-      return resetNotificationInTime(dispatch)
+      notifyError('Could not login', dispatch)
     })
   }
 }
-
-const search = (location, token) => {
-  console.log('got to search in action creator')
-  return () => {}
-  // return (dipatch) => {
-  //   backendInterface.search(location, token)
-  //   .then(({success, businesses}) => {
-  //     console.log(success)
-  //     console.log(businesses)
-  //   })
-  //   .catch(x => console.log(x))
-  // }
-}
-
 
 export {
   createUser,
   authenticate,
   logout,
-  setToken,
-  search
+  setToken
 }
