86537b22be63e6576cbb0f8ec66b59949a8b6df4
MouseZero <rm@mousezero.com>
2017-06-24 20:03:31 -0700

    fix some lint errors

--- src/components/top-bar.js
+++ src/components/top-bar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { search } from '../actions/user'
 import { setSearchInput, searchAction } from '../actions/search'
 import NightlifeButton from './NightlifeButton'
 
--- src/util/environmentCheck.js
+++ src/util/environmentCheck.js
@@ -1,7 +1,9 @@
 
 const checkEnvVar = (envVarResolved, nameOfVar) => {
   if (!envVarResolved) {
+    /* eslint-disable no-alert, no-console */
     console.log('Enviornment Varable ' + nameOfVar + ' is not set it is unlikly the program will work correctly')
+    /* eslint-enable no-alert, no-console */
   }
 }
 
