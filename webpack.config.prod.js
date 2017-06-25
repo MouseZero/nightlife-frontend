1bda5680c6e66bdcf1ff57e7b9448a3ca89bf529
MouseZero <rm@mousezero.com>
2017-06-24 21:48:58 -0700

    npm start now creates production server

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,6 +9,7 @@ import path from 'path'
 
 const GLOBALS = {
   'process.env.NODE_ENV': JSON.stringify('production'),
+  'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL),
   __DEV__: false
 }
 
